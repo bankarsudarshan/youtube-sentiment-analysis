28a4b8f892aeee506a8eb96768a2d5719cae3792
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2025-11-24 11:39:05 +0530

    added the data_preprocessing component which loads raw train/test data, preprocesses each comment thoroughly, and stores processed data in data/processed

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import os
-from sklearn.model_selection import train_test_split
 import logging
-import yaml
+import os
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 # Ensure the "logs" directory exists
