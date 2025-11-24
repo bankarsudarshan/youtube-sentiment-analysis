2da28b36cd80d9b36795b3caddd38df16704ed33
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2025-11-24 13:09:21 +0530

    model_building pipeline component added. This component takes the feature engineered data from data/feature-engineered and trains the hypertuned LightGBM model on the training data

--- src/model_building.py
+++ src/model_building.py
@@ -1,25 +1,21 @@
 import logging
 import os
 import pickle
-import yaml
 
+import lightgbm as lgb
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 # logging configuration
-log_dir = 'logs'
-os.makedirs(log_dir, exist_ok=True)
-
 logger = logging.getLogger('model_building')
 logger.setLevel('DEBUG')
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel('DEBUG')
 
-log_file_path = os.path.join(log_dir, 'feature_engineering.log')
-file_handler = logging.FileHandler(log_file_path)
+file_handler = logging.FileHandler('model_building.log')
 file_handler.setLevel('DEBUG')
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -44,19 +40,26 @@ def load_data(file_path: str) -> pd.DataFrame:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-def apply_tfidf(train_data: pd.DataFrame, max_features: int, ngram_range: tuple) -> tuple:
-    """Apply TF-IDF with ngrams to the data."""
+def train_lgbm(X_train: np.ndarray, y_train: np.ndarray, learning_rate: float, max_depth: int, n_estimators: int) -> lgb.LGBMClassifier:
+    """Train a LightGBM model."""
     try:
-        vectorizer = TfidfVectorizer(max_features=max_features, ngram_range=ngram_range)
-        X_train = train_data['clean_comment'].values
-        y_train = train_data['category'].values
-        X_train_tfidf = vectorizer.fit_transform(X_train) # TF-IDF transformation
-        train_df = pd.DataFrame(X_train_tfidf.toarray())
-        train_df['category'] = y_train
-        logger.debug(f"TF-IDF applied with bigrams training data transformed. Train shape: {train_df.shape}")
-        return train_df, vectorizer
+        best_model = lgb.LGBMClassifier(
+            objective='multiclass',
+            num_class=3,
+            metric="multi_logloss",
+            is_unbalance=True,
+            class_weight="balanced",
+            reg_alpha=0.1,  # L1 regularization
+            reg_lambda=0.1,  # L2 regularization
+            learning_rate=learning_rate,
+            max_depth=max_depth,
+            n_estimators=n_estimators
+        )
+        best_model.fit(X_train, y_train)
+        logger.debug('LightGBM model training completed')
+        return best_model
     except Exception as e:
-        logger.error('Error during TF-IDF transformation: %s', e)
+        logger.error('Error during LightGBM model training: %s', e)
         raise
 
 def save_model(model, file_path: str) -> None:
@@ -73,33 +76,23 @@ def save_model(model, file_path: str) -> None:
         logger.error('Error occurred while saving the model: %s', e)
         raise
 
-def save_data(df: pd.DataFrame, file_path: str) -> None:
-    """Save the dataframe to a CSV file."""
-    try:
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        df.to_csv(file_path, index=False)
-        logger.debug('Data saved to %s', file_path)
-    except Exception as e:
-        logger.error('Unexpected error occurred while saving the data: %s', e)
-        raise
-
-
 def main():
     try:
-        max_features = 1000
-        ngram_range = (1, 2)
+        # params
+        learning_rate, max_depth, n_estimators = 0.09, 20, 367
 
-        # Load the preprocessed training data
-        train_data = load_data('./data/processed/train_processed.csv')
+        # load the feature engineered training data 
+        train_tfidf = load_data('./data/feature-engineered/train_tfidf.csv')
+        X_train = train_tfidf.iloc[:, :-1].values
+        y_train = train_tfidf.iloc[:, -1].values
 
-        # Apply TF-IDF feature engineering on training data
-        train_tfidf, vectorizer = apply_tfidf(train_data, max_features, ngram_range)
+        # train the LightGBM model
+        lgbm = train_lgbm(X_train, y_train, learning_rate, max_depth, n_estimators)
 
-        save_data(train_tfidf, os.path.join("./data", "feature-engineered", "train_tfidf.csv"))
-        
-        model_save_path = 'models/tfidf_vectorizer.pkl'
-        save_model(vectorizer, model_save_path)
-        
+        # save the trained model in the root directory
+        model_save_path = 'models/lgbm_model.pkl'
+        save_model(lgbm, model_save_path)
+
     except Exception as e:
         logger.error('Failed to complete the feature engineering and model building process: %s', e)
         print(f"Error: {e}")
