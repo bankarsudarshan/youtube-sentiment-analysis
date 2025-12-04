9b76657be77a5d69f4363b06943675f6f4e381d5
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2025-12-04 15:03:48 +0530

    modified data_preprocessing component

--- src/components/data_preprocessing.py
+++ src/components/data_preprocessing.py
@@ -1,36 +1,12 @@
-import logging
-import os
-import string
 import re
 
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import numpy as np
 import pandas as pd
 
-
-# Ensure the "logs" directory exists
-log_dir = 'logs'
-os.makedirs(log_dir, exist_ok=True)
-
-# Setting up logger
-logger = logging.getLogger('data_preprocessing')
-logger.setLevel('DEBUG')
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel('DEBUG')
-
-log_file_path = os.path.join(log_dir, 'data_preprocessing.log')
-file_handler = logging.FileHandler(log_file_path)
-file_handler.setLevel('DEBUG')
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+from src.logger import logging
+from src.utils.main_utils import load_data, save_data
 
 
 # Download required NLTK data
@@ -40,61 +16,51 @@ nltk.download('stopwords')
 def preprocess_comment(comment):
     """Apply preprocessing transformations to a comment."""
     try:
-        comment = comment.lower() # Convert to lowercase
-        comment = comment.strip() # Remove trailing and leading whitespaces
-        comment = re.sub(r'\n', ' ', comment) # Remove newline characters
-        comment = re.sub(r'[^A-Za-z0-9\s!?.,]', '', comment) # Remove non-alphanumeric characters, except punctuation
+        comment = comment.lower() # to lowercase
+        comment = comment.strip() # trailing and leading whitespaces
+        comment = re.sub(r'\n', ' ', comment) # remove newline characters
+        comment = re.sub(r'[^A-Za-z0-9\s!?.,]', '', comment) # remove non-alphanumeric characters, except punctuation
 
         stop_words = set(stopwords.words('english')) - {'not', 'but', 'however', 'no', 'yet'}
-        comment = ' '.join([word for word in comment.split() if word not in stop_words]) # Remove unimportant stopwords
+        comment = ' '.join([word for word in comment.split() if word not in stop_words]) # remove unimportant stopwords
         
         lemmatizer = WordNetLemmatizer()
-        comment = ' '.join([lemmatizer.lemmatize(word) for word in comment.split()]) # Lemmatize
+        comment = ' '.join([lemmatizer.lemmatize(word) for word in comment.split()]) # lemmatize
         
         return comment
     except Exception as e:
-        logger.error(f"Error in preprocessing comment: {e}")
+        logging.error(f"Error in preprocessing comment: {e}")
         return comment
     
-def normalize_text(df):
+def normalize_text(df: pd.DataFrame):
     """Apply preprocessing to the text data in the dataframe."""
     try:
         df['clean_comment'] = df['clean_comment'].apply(preprocess_comment)
-        logger.debug('Text normalization completed')
+        logging.debug('Text normalization completed')
         return df
     except Exception as e:
-        logger.error(f"Error during text normalization: {e}")
-        raise
-
-def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
-    """Save the processed train and test datasets."""
-    try:
-        interim_data_path = os.path.join(data_path, 'processed')
-        os.makedirs(interim_data_path, exist_ok=True)
-        train_data.to_csv(os.path.join(interim_data_path, "train_processed.csv"), index=False)
-        test_data.to_csv(os.path.join(interim_data_path, "test_processed.csv"), index=False)
-        logger.debug(f"Processed train and test data saved to {interim_data_path}")
-    except Exception as e:
-        logger.error(f"Error occurred while saving data: {e}")
+        logging.error(f"Error during text normalization: {e}")
         raise
 
 def main():
     try:
-        logger.debug("Starting data preprocessing...")
+        logging.debug("Starting data preprocessing...")
         
         # Fetch the raw data
-        train_data = pd.read_csv('./data/raw/train.csv')
-        test_data = pd.read_csv('./data/raw/test.csv')
-        logger.debug('Raw train and test data loaded successfully')
+        train_data = load_data('artifacts/data_ingestion/ingested/train.csv')
+        test_data = load_data('artifacts/data_ingestion/ingested/test.csv')
+        logging.debug('Raw train and test data loaded successfully')
 
         # Preprocessing
         train_processed_data = normalize_text(train_data)
         test_processed_data = normalize_text(test_data)
 
         # Save
-        save_data(train_processed_data, test_processed_data, data_path='./data')
+        save_data(train_processed_data, 'artifacts/data_preprocessing/train_processed.csv')
+        save_data(test_processed_data, 'artifacts/data_preprocessing/test_processed.csv')
+
     except Exception as e:
-        logger.error('Failed to complete the data preprocessing process: %s', e)
+        logging.error(f'Failed to complete the data preprocessing process: {e}')
         print(f"Error: {e}")
 
 
